1e8c1cdcd232cf4eb66e0c98ff86c226e1b34dda
Nguyễn Công Tuấn Khanh (chuyên đề thực tập) <128701713+themco306@users.noreply.github.com>
2023-10-17 07:14:02 +0700

    Delete web_frontend_admin directory

--- web_frontend_admin/src/redux/actions/titleAction.jsx
+++
@@ -1,8 +0,0 @@
-import { TITLE_SET } from "./actionTypes"
-
-export const setTitle=(title)=>(dispatch)=>{
-    dispatch({
-        type:TITLE_SET,
-        payload:title
-    })
-}
--- web_frontend_admin/src/services/constant.jsx
+++
@@ -1,3 +0,0 @@
-export const API_CATEGORY='http://localhost:8080/api/categories'
-export const API_BRAND='http://localhost:8080/api/brands'
-export const API_PRODUCT='http://localhost:8080/api/products'
