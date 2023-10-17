2c01555ff279d074cffbe6c8b3f57fcb463f5cdf
Nguyễn Công Tuấn Khanh (chuyên đề thực tập) <128701713+themco306@users.noreply.github.com>
2023-10-17 07:44:30 +0700

    Delete resources directory

--- resources/views/components/post-item.blade.php
+++
@@ -1,8 +0,0 @@
-
-    <a href="{{ route('slug.index',['slug'=>$post->slug]) }}">
-    <div class="img-wrapper">
-        <img class="img-fluid" src="{{ asset('images/post/'.$post->image) }}" alt="{{ $post->image }}">
-    </div>
-        <h5 class="title">{{ $post->title }}</h5>
-    </a>
-        {{-- <span class="text-center">{!! Str::words($post->detail,15, '  [...]') !!}</span> --}}
--- resources/views/components/site-footer-menu.blade.php
+++
@@ -1,8 +0,0 @@
-
-@foreach($list_menu as $fmenu)
-<li >
-    <a href="{{ route('slug.index',['slug'=>$fmenu->link]) }}">
-        {{ $fmenu->name }}
-    </a>
-</li>
-@endforeach
